6c5aa1c6a223043ac789ccc0f4d83e96e6c38215
Alexander Olvera <alexander.olvera943@gmail.com>
2022-01-13 16:18:47 -0600

    Added auth protection to dashboard

--- pages/[uid]/dashboard.js
+++ pages/[uid]/dashboard.js
@@ -3,10 +3,16 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Stack from 'react-bootstrap/Stack'
+import Button from 'react-bootstrap/Button'
+
+import { auth } from '../../lib/firebase'
+import { signOut } from 'firebase/auth'
+
 import {
   useAuthUser,
   withAuthUser,
   withAuthUserTokenSSR,
+  AuthAction
 } from 'next-firebase-auth'
 
 const Dashboard = function () {
@@ -14,10 +20,15 @@ const Dashboard = function () {
   const router = useRouter()
   const { uid } = router.query
 
+  const handleSignOut = async () => {
+    await signOut(auth)
+  }
+
   return (
     <>
       <h3>Dashboard Component</h3>
       <p>Your email is {AuthUser.email ? AuthUser.email : 'unknown'}.</p>
+      <Button onClick={handleSignOut} variant='error'>SignOut</Button>
       <Container>
         <Row>
           <Col md={4} >
@@ -44,4 +55,8 @@ const Dashboard = function () {
 
 export const getServerSideProps = withAuthUserTokenSSR()()
 
-export default withAuthUser()(Dashboard)
+export default withAuthUser({
+  whenUnauthedBeforeInit: AuthAction.SHOW_LOADER,
+  whenUnauthedAfterInit: AuthAction.REDIRECT_TO_LOGIN,
+  authPageURL: '/login',
+})(Dashboard)

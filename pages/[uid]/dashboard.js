af748d0aa6d06adaaa21c3ad13139a2e58915e4a
Alexander Olvera <alexander.olvera943@gmail.com>
2022-01-13 16:05:55 -0600

    Added next-firebase-auth for using auth with ssr and csr

--- pages/[uid]/dashboard.js
+++ pages/[uid]/dashboard.js
@@ -1,13 +1,47 @@
 import { useRouter } from 'next/router'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Stack from 'react-bootstrap/Stack'
+import {
+  useAuthUser,
+  withAuthUser,
+  withAuthUserTokenSSR,
+} from 'next-firebase-auth'
 
-export default function Dashboard() {
+const Dashboard = function () {
+  const AuthUser = useAuthUser()
   const router = useRouter()
   const { uid } = router.query
 
   return (
     <>
       <h3>Dashboard Component</h3>
-      <h6>{uid}</h6>
+      <p>Your email is {AuthUser.email ? AuthUser.email : 'unknown'}.</p>
+      <Container>
+        <Row>
+          <Col md={4} >
+            <h6>My Campaigns</h6>
+            <Stack gap={2} className='border rounded shadow p-2'>
+              <div className="bg-light border rounded">First item</div>
+              <div className="bg-light border rounded">Second item</div>
+              <div className="bg-light border rounded">Third item</div>
+            </Stack>
+          </Col>
+          <Col md={{ span: 4, offset: 4 }} >
+            <h6>My Upcoming Adventures</h6>
+            <Stack gap={2} className='border rounded shadow p-2'>
+              <div className="bg-light border rounded">First item</div>
+              <div className="bg-light border rounded">Second item</div>
+              <div className="bg-light border rounded">Third item</div>
+            </Stack>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
+
+export const getServerSideProps = withAuthUserTokenSSR()()
+
+export default withAuthUser()(Dashboard)
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import initAuth from '../lib/initAuth';
 import Layout from '../components/Layout'
+
+initAuth()
 
 function MyApp({ Component, pageProps }) {
   return (

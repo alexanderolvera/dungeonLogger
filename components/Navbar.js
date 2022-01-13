b4fb3862789fe12f7e63bd64807243bee1f4f156
Alexander Olvera <alexander.olvera943@gmail.com>
2022-01-13 14:42:12 -0600

    Removed tailwind for react-bootstrap

---
+++ components/Navbar.js
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+
+export default function NavBar() {
+  return (
+    <Navbar bg="dark" expand="lg" variant="dark">
+      <Container>
+        <Link href='/' passHref>
+          <Navbar.Brand>Dungeon Logger</Navbar.Brand>
+        </Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Link href={'/About'} passHref>
+              <Nav.Link>About</Nav.Link>
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
